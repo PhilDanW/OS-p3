52ff067f5130f1eca3ae07330ebacd11990a8491
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-14 15:37:05 -0500

    Create monitor.h

---
+++ monitor.h
@@ -0,0 +1,11 @@
+#ifndef MONITOR_H
+#define MONITOR_H
+
+#include <iostream>
+using namespace std;
+
+int monitor(string, int, int, int);
+int fork(string, string, int arrayItem = -1);
+void allocateMemory();
+
+#endif
