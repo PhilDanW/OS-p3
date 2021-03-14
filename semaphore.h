b0a07deb2e108026baedf17efc89db1e90de11ee
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-14 18:12:06 -0500

    Create semaphore.h

---
+++ semaphore.h
@@ -0,0 +1,24 @@
+#ifndef SEMAPHORES
+#define SEMAPHORES
+
+#include <sys/sem.h>
+
+class semaphores
+{
+    public:
+        semaphores(key_t, bool, int = 1);
+        ~semaphores();
+        // Check if properly setup
+        bool isInitialized() { return _isInitialized; };
+        // Semaphore Wait
+        void Wait();
+        // Semaphore Signal
+        void Signal();  
+    private:
+        bool _bCreator;
+        int _semid;
+        bool _isInitialized;
+        struct sembuf structSemaBuf;
+};
+
+#endif
