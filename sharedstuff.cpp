497e649b275275431f5ac46fc13f062afd734351
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-14 16:36:29 -0500

    Update sharedstuff.cpp

--- sharedstuff.cpp
+++ sharedstuff.cpp
@@ -99,14 +99,3 @@ bool WriteToLog(std::string& logString, std::string LogFile)
         return false;
     }
 }
-
-static void prog_use(std::string name)
-{
-   std::cerr << std::endl << "\t" << name << " [-h] [-o logfile] [-p m] [-c n] [-t time]" << std::endl
-   << "Input options:" << std::endl
-   << "-o 'logfile' Name of file to keep program logs" << std::endl
-   << "-p 'x' number of producers to make" << std::endl
-   << "-c 'y' number of consumers to make" << std::endl
-   << "-t 'time' seconds after which the process will terminate"<< std::endl 
-   << std::endl;
-}
