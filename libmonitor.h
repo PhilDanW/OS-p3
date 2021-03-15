f72e10e629733721e2daf79cdcac5d336530d5c3
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-15 18:33:53 -0500

    Update libmonitor.h

--- libmonitor.h
+++ libmonitor.h
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int monitor(string, int, int, int);
+int monitorProcess(string, int, int, int);
 int fork(string, string, int arrayItem = -1);
 void allocateMemory();
 
