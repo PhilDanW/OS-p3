bb99c546c0dce0d1faafbd8a960b2cf3ec0d810b
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-14 20:01:38 -0500

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int monitor(string, int, int, int);
+int monitor(int, int, int);
 int fork(string, string, int arrayItem = -1);
 void allocateMemory();
 
