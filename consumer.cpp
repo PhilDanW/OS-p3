d068d38baf69eeb082b65fd3415d52e44c14a7eb
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-03-15 19:15:29 -0500

    Update consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -1,13 +1,16 @@
+//Philip Wright
+//CMP4760 - Project3
+//the part of the program controls the functions of the consumers and begins in the monitor process
 #include <iostream>
 #include <unistd.h>
 #include "sharedStructures.h"
 #include "productSemaphores.h"
 #include <fstream>
 
-// Forward Declarations
+//declare the usage function
 static void show_usage(std::string);
 
-// SIGQUIT handling
+//establish the signal handler
 volatile sig_atomic_t sigQuitFlag = 0;
 void sigQuitHandler(int sig){ // can be called asynchronously
   sigQuitFlag = 1; // set flag
@@ -17,70 +20,66 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  // Get the Index to the array assigned to this
-  // consumer to process
+  // Get the Index to the array assigned to this consumer
   if(argc < 2)
   {
     cout << "Args: " << argv[0] << endl;
-    perror("Consumer: Incorrect argument found");
+    perror("Consumer: invalid argument found");
     exit(EXIT_FAILURE);
   }
-  // We can now get the index to the item to process
+  // get the index of the item to process
   const int nItemToProcess = atoi(argv[1]);
 
-  // And the log file string
+  // get the log file string
   string strLogFile = argv[2];
 
   // Seed the randomizer
   srand(time(NULL));
 
-  // Register SIGQUIT handling
+  // Register the signal handler
   signal(SIGINT, sigQuitHandler);
 
   // Log startup of the child
   const pid_t nPid = getpid();
-  string strLog = "Consumer: PID ";
+  string strLog = "The Consumer PID is: ";
   strLog.append(GetStringFromInt(nPid));
   strLog.append(" Started");
   WriteLogFile(strLog, strLogFile);
 
-  // Find the necessary Semaphores
+  // get the necessary Semaphores
   productSemaphores s(KEY_MUTEX, false);
   productSemaphores n(KEY_EMPTY, false);
   productSemaphores e(KEY_FULL, false);
 
   if(!s.isInitialized() || !n.isInitialized() || !e.isInitialized())
   {
-    perror("Consumer: Could not successfully find Semaphores");
+    perror("consumer process: Could not find Semaphores");
     exit(EXIT_FAILURE);
   }
 
-  // Open the connection to shared memory
   // Allocate the shared memory
-  // And get ready for read/write
-  // Get a reference to the shared memory, if available
   shm_id = shmget(KEY_SHMEM, 0, 0);
   if (shm_id == -1) {
-      perror("Consumer: Could not successfully find Shared Memory");
+      perror("consumer process: Could not find Shared Memory");
       exit(EXIT_FAILURE);
   }
 
-  // Read the memory size and calculate the array size
+  // Read the memory size
   struct shmid_ds shmid_ds;
   shmctl(shm_id, IPC_STAT, &shmid_ds);
   size_t realSize = shmid_ds.shm_segsz;
 
-  // Now we have the size - actually setup with shmget
+  //setup with shmget
   shm_id = shmget(KEY_SHMEM, realSize, 0);
   if (shm_id == -1) {
-      perror("Consumer: Could not successfully find Shared Memory");
+      perror("consumer process: Could not find Shared Memory");
       exit(EXIT_FAILURE);
   }
 
-  // attach the shared memory segment to our process's address space
+  // attach the shared memory segment
   shm_addr = (char*)shmat(shm_id, NULL, 0);
   if (!shm_addr) { /* operation failed. */
-      perror("Consumer: Could not successfully attach Shared Memory");
+      perror("consumer process: Could not attach Shared Memory");
       exit(EXIT_FAILURE);
   }
 
@@ -91,24 +90,18 @@ int main(int argc, char* argv[])
   struct ProductItem*productItemQueue = 
       (struct ProductItem*) (shm_addr+sizeof(int)+sizeof(productHeader));
 
-  // Get a random time to sleep between 1-10 seconds
+  // Get a random time to sleep
   int nSleepTime = rand()%10+1;
 
-  // Sleep for the random time
+  // Sleep for the time
   while(!sigQuitFlag && !nSleepTime)
   {
     sleep(nSleepTime);
     nSleepTime--;
   }
-
-  // Get Exclusive Access via Semaphores
+  
   n.Wait();
   s.Wait();
-
-  // Debug print queue
-  //for(int i=0;i<productHeader->QueueSize;i++ )
-  //  cout << productItemQueue[i].itemValue << " ";
-  //cout << endl;
 
   // Consume the value
   float fNewPiVal =
@@ -125,7 +118,7 @@ int main(int argc, char* argv[])
   strLog.append(GetStringFromInt(nItemToProcess));
   WriteLogFile(strLog, strLogFile);
 
-  cout << "Consumer: " << nPid << " consumed item in queue: " << nItemToProcess << endl;
+  cout << "Consumer PID is: " << nPid << " consumed item in queue: " << nItemToProcess << endl;
 
   s.Signal();
   e.Signal();
